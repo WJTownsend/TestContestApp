39c35e300612714c1dd9a0744a12aec9f689166c
Joe Townsend <TownsendSignUp@gmail.com>
2022-10-04 23:27:00 -0600

    Correct quotes around f-string variables

--- TESTStreamLit.py
+++ TESTStreamLit.py
@@ -278,7 +278,7 @@ def new_contest_entry():
             sheet_url = st.secrets["private_gsheets_url"]
 
             # Troubleshooting for "Couldn't extract column labels from sheet"
-            test_query = f"SELECT * FROM {sheet_url}"
+            test_query = f'SELECT * FROM "{sheet_url}"'
             cursor.execute(test_query)
 
             query = f'INSERT INTO "{sheet_url}" VALUES ("{entrant_name}", "{entrant_discord}", "{submission_time}", "{q1a1}", "{q1a2}", "{q1a3}", "{q1a4}", "{q1a5}", "{q2a1}", "{q2a2}", "{q2a3}", "{q2a4}", "{q2a5}", "{q3a1}", "{q3a2}", "{q3a3}", "{q3a4}", "{q3a5}", "{q4a1}", "{q4a2}", "{q4a3}", "{q4a4}", "{q4a5}", "{q5a1}", "{q5a2}", "{q5a3}", "{q5a4}", "{q5a5}", "{q6a1}", "{q6a2}", "{q6a3}", "{q6a4}", "{q6a5}", "{q7a1}", "{q7a2}", "{q7a3}", "{q7a4}", "{q7a5}", "{q8a1}", "{q8a2}", "{q8a3}", "{q8a4}", "{q8a5}", "{q9a1}", "{q9a2}", "{q9a3}", "{q9a4}", "{q9a5}", "{q10a1}", "{q10a2}", "{q10a3}", "{q10a4}", "{q10a5}")'
@@ -315,7 +315,7 @@ def retrieve_last_entry():
             })
             cursor = connection.cursor()
             sheet_url = st.secrets["private_gsheets_url"]
-            query = f'SELECT * FROM {sheet_url} WHERE B = {entrant_discord}'
+            query = f'SELECT * FROM "{sheet_url}" WHERE B = {entrant_discord}'
             st.write(f"These are the entries that have been submitted for the contest using the handle {entrant_discord}:")
             for row in cursor.execute(query):
                 print(row)
